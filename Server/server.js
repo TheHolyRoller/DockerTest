5610c3f18dbd56b20f2fe04e2199a54679b26af7
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2024-05-26 01:00:15 +0100

    .

--- Server/server.js
+++ Server/server.js
@@ -1,21 +1,26 @@
 const express = require('express');
-const app = express();
 const cors = require('cors'); 
 
-app.use(cors());
+// IP ADDRESS
+// 172.17.0.2
+
+const app = express();
+
+
+app.use(cors())
+
 app.use((req, res, next) => {
-  res.setHeader('Content-Security-Policy', '');
+  res.setHeader('Content-Security-Policy', "default-src 'self' *");
   next();
 });
 
-app.get('/upload', (req, res) => {
-    // Your code here
-    const payload = req.body;
-    console.log("this is the payload", payload);
-    res.setHeader('Content-Security-Policy', "default-src 'self' http://localhost:3000");
- 
-    res.json({ message: "Testing" });
 
+
+
+app.get('/', (req, res) => {
+    // Your code here
+
+    res.json({ message: 'Hello World!' });
   });
   
 
